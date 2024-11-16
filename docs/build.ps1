28831df853f082ec3d82cc785b214767e84a202e
Shawn Tan <banchan@gmail.com>
2024-11-15 16:54:41 -0800

    Modify GitHub actions and build.ps1 to support Workflow patch

--- docs/build.ps1
+++ docs/build.ps1
@@ -1,2 +1,4 @@
 .\bonsai\modules\Export-Image.ps1 "..\src\BonVision\bin\Release\net462"
-dotnet docfx @args
+dotnet docfx metadata
+python ./bonsai/template/api/plugins/Patch-IncludeWorkflow.py
+dotnet docfx build
